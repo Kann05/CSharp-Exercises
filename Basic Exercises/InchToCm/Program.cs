8bc53c413a20de81322df2cecdc86b25e719ee72
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:19:36 +0300

    Add files via upload

---
+++ Basic Exercises/InchToCm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+        Console.Write("Inches: ");
+		double inch = Convert.ToInt32(Console.ReadLine());
+        double centimeters = inch * 2.54;
+
+        System.Console.Write($"{centimeters} centimeters");
+	}
+}
