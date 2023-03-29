e4ff4d915ab5a788f31cd7e8ecdb413be2001eb9
Erkan <zapochvamdneska@gmail.com>
2023-03-29 20:44:51 +0300

    Add files via upload

---
+++ OOP/Cash Receipt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class CashReceipt{
+
+    public void Head(){
+        System.Console.WriteLine("CASH RECEIPT");
+        System.Console.WriteLine("------------------------------");
+    }
+    public void Body(){
+        System.Console.WriteLine("Charged to____________________");
+        System.Console.WriteLine("Received by___________________");
+    }
+    public void End(){
+        System.Console.WriteLine("------------------------------");
+        System.Console.WriteLine("© AlpSer");
+
+    }
+    public void Call(){
+        Head();
+        Body();
+        End();
+    }
+}
+public class Program
+{
+	public static void Main()
+	{
+		CashReceipt receipt = new CashReceipt();
+        receipt.Call();
+	}
+}
---
+++ OOP/Class Person exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Alphabet
+{
+    class Person{
+        public string name;
+        public int age;
+        public void Introducing(){
+            name = Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine(name);
+            System.Console.WriteLine(age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Introducing();          
+        }
+    }
+}
