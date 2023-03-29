e5ec8af3a749b7a992693a1684c02a8d20e0bd13
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:04:20 +0300

    Add files via upload

---
+++ Array Exercises/Bubble Sorting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		int[] array = new int[] {5, 1, 4, 10, 3, 2, 7, 0, 6};
+        int[] distinctArray = array.Distinct().ToArray();
+        int temp;
+
+        for (int i = 0; i < array.Length; i++){
+            for (int j = 0; j < array.Length -1; j++){
+                
+                if (array[j] > array[j + 1]){
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        foreach (int arr in array){
+            System.Console.Write(arr);
+        }
+	}
+}
---
+++ Array Exercises/MultiDimensional Array sum/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter the number of rows:");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number of columns:");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, cols];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.WriteLine("Enter the value for element ({0},{1}):", i, j);
+        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int[] rowSums = new int[rows];
+int[] colSums = new int[cols];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        rowSums[i] += matrix[i, j];
+        colSums[j] += matrix[i, j];
+    }
+}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write("{0} ", matrix[i, j]);
+    }
+    Console.WriteLine("Row {0} sum = {1}", i, rowSums[i]);
+}
+
+for (int j = 0; j < cols; j++)
+{
+    Console.WriteLine("Column {0} sum = {1}", j, colSums[j]);
+}
+
+int totalSum = rowSums.Sum();
+Console.WriteLine("Total sum of all elements = {0}", totalSum);
