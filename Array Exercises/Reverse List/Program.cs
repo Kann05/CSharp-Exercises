a3664987fa1cc31657a446c11b524f1f1d583090
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:01:20 +0300

    Add files via upload

---
+++ Array Exercises/Reverse List/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		string nums = Console.ReadLine();
+        List<string> names = nums.Split(' ').ToList();
+
+        names.Reverse();
+
+        foreach(string i in names){
+            System.Console.WriteLine($"{i}; ");
+        }
+	}
+}
