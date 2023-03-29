41e9c61dee755daba5b27448c680db70f0c610f1
Erkan <zapochvamdneska@gmail.com>
2023-03-29 20:51:13 +0300

    Add files via upload

---
+++ Harder Exercises/Points Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PointsCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your points: ");
+            double points = int.Parse(Console.ReadLine());
+
+            if (points > 100)
+            {
+                points += 5;
+            }
+
+            if (points > 101)
+            {
+                points += points * 0.2;
+            }
+
+            if (points > 1000)
+            {
+                points += points * 0.1;
+            }
+
+            if (points % 2 == 0)
+            {
+                points += 1;
+            }
+
+            if (points % 10 == 5)
+            {
+                points += 2;
+            }
+
+            Console.WriteLine("Your total points are: " + points);
+        }
+    }
+}
---
+++ Harder Exercises/Seconds Summs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TimeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            int third = int.Parse(Console.ReadLine());
+
+            int totalSeconds = first + second + third;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+
+            Console.WriteLine("Time: " + minutes + " minutes " + (seconds < 10 ? "0" : "") + seconds + " seconds");
+        }
+    }
+}
