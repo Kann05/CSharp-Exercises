7e7aba463331dae51d37f84baae88cbe5722a0b0
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:13:59 +0300

    Add files via upload

---
+++ Basic Exercises/Sum numbers Biggest number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		System.Console.Write("Enter how many numbers to sum: ");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        int max = int.MinValue;
+
+        for (int i = 1; i <= count; i++){
+            System.Console.Write(i + ".");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+
+            sum += numbers;
+            max = Math.Max(max, numbers);
+
+        }
+
+        System.Console.WriteLine($"Your total value is : {sum}");
+        System.Console.WriteLine($"The maximum number is {max}");
+	}
+}
