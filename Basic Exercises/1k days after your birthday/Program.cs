678a497c96c7793836c003c39c5524e83b532750
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:09:08 +0300

    Add files via upload

---
+++ Basic Exercises/1k days after your birthday/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BirthdayCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your birthday (dd/mm/yyyy): ");
+            string birthdayInput = Console.ReadLine();
+
+            DateTime birthday = DateTime.ParseExact(birthdayInput, "dd/MM/yyyy", null);
+            DateTime thousandDaysLater = birthday.AddDays(1000);
+
+            Console.WriteLine("1000 days after your birthday is: " + thousandDaysLater.ToString("dd/MM/yyyy"));
+        }
+    }
+}
---
+++ Basic Exercises/Beep sound/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		Console.Beep(432, 500);
+	}
+}
