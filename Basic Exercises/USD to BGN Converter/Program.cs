cd3ee09b75bac9cb504b65fc1b5a026e24aecae7
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:12:06 +0300

    Add files via upload

---
+++ Basic Exercises/USD to BGN Converter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double usd;
+        double bgn;
+        double rate = 1.7; // current exchange rate (1 USD = 1.7 BGN)
+
+        Console.Write("Enter amount in USD: ");
+        usd = double.Parse(Console.ReadLine());
+
+        bgn = usd * rate;
+
+        Console.WriteLine("Amount in BGN: {0}", bgn);
+    }
+}
