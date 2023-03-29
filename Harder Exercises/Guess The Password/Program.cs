462f9620b657c905926bde0b66038299f21f99d5
Erkan <zapochvamdneska@gmail.com>
2023-03-29 20:54:27 +0300

    Add files via upload

---
+++ Harder Exercises/Guess The Password/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		string password = "random";
+        string guessPassword;
+
+        System.Console.Write("Guess the password = ");
+        guessPassword = Console.ReadLine().ToLower();
+
+        if (password == guessPassword){
+            System.Console.WriteLine("Congrats");
+        }
+        else {
+            System.Console.WriteLine("Failed");
+        }
+	}
+}
