28c56c3a353e7198f60ffdf7f10e6bc1e5f3844f
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:26:40 +0300

    Add files via upload

---
+++ Basic Exercises/Fruit or Vegetable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a name: ");
+            string input = Console.ReadLine();
+
+            string result = "unknown";
+
+            switch (input)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "grapes":
+                    result = "fruit";
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    result = "vegetable";
+                    break;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
