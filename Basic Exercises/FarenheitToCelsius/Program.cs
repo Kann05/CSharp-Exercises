f98e2e2d7a7ab497522684f1e6192a1db04f9153
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:21:10 +0300

    Add files via upload

---
+++ Basic Exercises/FarenheitToCelsius/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double fahrenheit;
+        double celsius;
+
+        Console.Write("Enter temperature in Fahrenheit: ");
+        fahrenheit = double.Parse(Console.ReadLine());
+
+        celsius = (fahrenheit - 32) * 5 / 9;
+
+        Console.WriteLine("Temperature in Celsius: {0}", celsius);
+    }
+}
