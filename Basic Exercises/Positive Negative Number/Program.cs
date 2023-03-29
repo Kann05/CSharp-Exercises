d7898eccea3be2566b267f4610f4ed5627e430cf
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:15:29 +0300

    Add files via upload

---
+++ Basic Exercises/Positive Negative Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class PositiveNegative{
+    public void GetUserInfo(out int number){
+        System.Console.Write("Enter number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    public void PositiveOrNegative(int number){
+        if (number == 0){
+            System.Console.WriteLine("The number is Zero");
+        }
+        else if (number > 0){
+            System.Console.WriteLine("The number is Positive");
+        }
+        else {
+            System.Console.WriteLine("The number is Negative");
+        }
+    }
+    public void Calling(){
+        int number;
+        GetUserInfo(out number);
+        PositiveOrNegative(number);
+    }
+}
+public class Program
+{
+	public static void Main()
+	{
+		PositiveNegative call = new PositiveNegative();
+        call.Calling();
+	}
+}
---
+++ Basic Exercises/Speed Information/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		System.Console.Write("Enter your speed : ");
+        double speed = Convert.ToDouble(Console.ReadLine());
+
+        if (speed <= 10){
+            System.Console.WriteLine("Slow");
+        }
+        else if (speed <= 50 ){
+            System.Console.WriteLine("Average");
+        }
+        else if (speed <= 150 ){
+            System.Console.WriteLine("Fast");
+        }
+        else if (speed <= 1000 ){
+            System.Console.WriteLine("Ulta Fast");
+        }
+        else if (speed > 1000 ){
+            System.Console.WriteLine("Extremely Fast");
+        }
+	}
+}
