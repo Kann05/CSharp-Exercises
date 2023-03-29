0e0b140a91413e13c6f3c2ee37d7514895986acb
Erkan <zapochvamdneska@gmail.com>
2023-03-29 21:16:56 +0300

    Add files via upload

---
+++ Basic Exercises/Math abs exercise/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		System.Console.Write("Write number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine(Math.Abs(number));
+	}
+}
