6085d0a007fd9090ecf3a65685cf0f0a1dbbca96
Erkan <zapochvamdneska@gmail.com>
2023-04-14 18:19:34 +0300

    exercises

---
+++ 11 Class/Templates/Generics calculation/Program.cs
@@ -0,0 +1,20 @@
+﻿class Calculation<T>
+{
+    public bool numberIsHigherThan100(T num1)
+    {
+        if (num1.Equals(100))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        var calculate = new Calculation<int>();
+        calculate.numberIsHigherThan100(100);
+
+    }
+}
---
+++ 11 Class/Templates/Generics exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Nullable<T> where T : struct
+{
+    private object _value;
+    public Nullable()
+    {
+        
+    }
+    public Nullable(T value)
+    {
+     _value = value;    
+    }
+    public bool HasValue
+    {
+        get { return _value != null;}
+    }
+
+    public T GetValueOrDefault()
+    {
+        if (HasValue)
+        {
+            return (T)_value;
+        }
+        
+        return default(T);
+    }
+
+
+}
+class Program
+{
+    public static void Main()
+    {
+        var check = new Nullable<int>(5);
+        System.Console.WriteLine("Has Value ? " + check.HasValue);
+    }
+}
