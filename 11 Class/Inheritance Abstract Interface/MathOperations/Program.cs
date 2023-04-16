8bdcefe29b9c215dd9c1f53f8f663a868da4a541
Erkan <zapochvamdneska@gmail.com>
2023-04-16 21:49:20 +0300

    exercise

---
+++ 11 Class/Inheritance Abstract Interface/MathOperations/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Operations
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    public decimal Add(decimal a, decimal b, decimal c)
+    {
+        return a + b + c;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Operations operation = new Operations();
+
+        System.Console.WriteLine(operation.Add(2, 10));
+        System.Console.WriteLine(operation.Add(20d, 12d, 51d));
+        System.Console.WriteLine(operation.Add(31m, 21m, 41m));
+    }
+}
