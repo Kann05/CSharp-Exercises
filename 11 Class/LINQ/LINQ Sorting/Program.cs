fabe04c5981e55e08c6a70c83387f37c5b1aef52
Erkan <zapochvamdneska@gmail.com>
2023-04-17 16:57:31 +0300

    exercises

---
+++ 11 Class/LINQ/LINQ Sorting/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        List<int> nums = new List<int>{ 10, 22, 1, 4};
+
+        nums = nums
+        .OrderBy(num => num)
+        .ToList();
+
+        foreach(int num in nums)
+        {
+            System.Console.WriteLine(num);
+        }
+    }
+}
