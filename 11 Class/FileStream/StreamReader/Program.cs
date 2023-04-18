cf81bb21308d958d42e5397c86cb8c6d532c11d4
Erkan <zapochvamdneska@gmail.com>
2023-04-18 20:59:43 +0300

    exercises

---
+++ 11 Class/FileStream/StreamReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"..\streamReader.txt";
+
+            using (StreamReader read = new StreamReader(fileName))
+            {
+                Console.WriteLine(read.ReadLine());
+            }
+        }
+    }
+}
---
+++ 11 Class/FileStream/StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is an example of creating manually txt file in bin and writing in it
+            string manuallFileCreating = @"..\streamWriter.txt";
+
+            using (StreamWriter write = new StreamWriter(manuallFileCreating))
+            {
+                write.Write("Thats how you manually create a txt file and write something in it using StreamWriter");
+            }
+
+            //This is an example of auto creating txt file in bin and writing in it
+
+            FileStream autoFileCreating = new FileStream(@"..\autoFileCreating.txt", FileMode.OpenOrCreate);
+
+            using (StreamWriter reader = new StreamWriter(autoFileCreating))
+            {
+                reader.Write("Thats how you auto create a txt file and write something in it using StreamWriter");
+            }
+        }
+    }
+}
