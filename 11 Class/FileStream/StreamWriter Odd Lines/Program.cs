3930b8ea03ef2c38fce12598905532268302223e
Erkan <zapochvamdneska@gmail.com>
2023-04-19 22:46:53 +0300

    exercise

---
+++ 11 Class/FileStream/StreamWriter Odd Lines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader(@"..\file.txt"))
+            {
+                string line;
+                int count = 1;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (count % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    count++;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
