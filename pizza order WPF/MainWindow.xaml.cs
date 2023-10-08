906faf72025ff606dbbd5ddec41d614e05da8612
Erkan <zapochvamdneska@gmail.com>
2023-10-08 23:20:30 +0300

    exercises

---
+++ pizza order WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pizza_order
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private void Order(object sender, RoutedEventArgs e)
+        {
+            string selectedPizzaSize = GetSelectedRadioButtonContent(sizeOptions);
+            string selectedPaymentMethod = GetSelectedRadioButtonContent(paymentType);
+            string selectedBonus = GetSelectedRadioButtonContent(Bonus);
+
+            
+
+            Order orderPizza = new Order();
+            orderPizza.SetOrderInfo(selectedPizzaSize, selectedPaymentMethod, selectedBonus);
+            orderPizza.Show();
+            Close();
+        }
+
+        private string GetSelectedRadioButtonContent(ListBox listBox)
+        {
+            RadioButton selectedRadioButton = listBox.Items.OfType<RadioButton>().FirstOrDefault(rb => rb.IsChecked == true);
+            return selectedRadioButton != null ? selectedRadioButton.Content.ToString() : "";
+        }
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+    }
+}
---
+++ pizza order WPF/Order.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pizza_order
+{
+    /// <summary>
+    /// Interaction logic for Order.xaml
+    /// </summary>
+    public partial class Order : Window
+    {
+        public void SetOrderInfo(string size, string payment, string bonus)
+        {
+            sizeTextBlock.Text = $"Selected Pizza Size: {size}";
+            paymentTextBlock.Text = $"Selected Payment Method: {payment}";
+            bonusTextBlock.Text = $"Selected Bonus: {bonus}";
+        }
+        public Order()
+        {
+            InitializeComponent();
+        }
+    }
+}
